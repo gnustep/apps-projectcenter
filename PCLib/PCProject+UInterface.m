5ae48c71904507016bc2fe814f0c4f7a3d41a47b
Philippe C.D. Robert <probert@siggraph.org>
2002-11-27 18:41:07 +0000

    Updated the headers and copyright notice
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/trunk@15153 72102866-910b-0410-8b05-ffd578937521

--- PCLib/PCProject+UInterface.m
+++ PCLib/PCProject+UInterface.m
@@ -1,10 +1,28 @@
-//
-//  PCProject+UInterface.m
-//  ProjectCenter
-//
-//  Created by Philippe C.D. Robert on Wed Nov 27 2002.
-//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
-//
+/*
+   GNUstep ProjectCenter - http://www.gnustep.org
+
+   Copyright (C) 2000-2002 Free Software Foundation
+
+   Author: Philippe C.D. Robert <probert@siggraph.org>
+
+   This file is part of GNUstep.
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+
+   $Id$
+*/
 
 #import "PCProject+UInterface.h"
 #import "PCSplitView.h"
--- PCLib/PCProject.h
+++ PCLib/PCProject.h
@@ -1,7 +1,7 @@
 /*
    GNUstep ProjectCenter - http://www.gnustep.org
 
-   Copyright (C) 2001 Free Software Foundation
+   Copyright (C) 2000-2002 Free Software Foundation
 
    Author: Philippe C.D. Robert <probert@siggraph.org>
 
