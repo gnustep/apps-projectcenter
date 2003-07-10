237bddc7c0298249c733c8eed445af12d3646877
Philippe C.D. Robert <probert@siggraph.org>
2003-07-10 10:23:01 +0000

    Added missing includes
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/branches/RELEASE_0_3_5@17181 72102866-910b-0410-8b05-ffd578937521

--- PCLib/PCButton.m
+++ PCLib/PCButton.m
@@ -25,6 +25,7 @@
  */
 
 #include "PCButton.h"
+#include "PCDefines.h"
 
 @implementation PCButton
 
--- PCLib/PCDataSource.m
+++ PCLib/PCDataSource.m
@@ -25,6 +25,7 @@
 */
 
 #include "PCDataSource.h"
+#include "PCDefines.h"
 
 @implementation PCDataSource
 
