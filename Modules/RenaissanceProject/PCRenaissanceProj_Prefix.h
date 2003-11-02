3a5e704aad97b44f0d52c420b96ddaea44e97c34
Sergii Stoian <stoyan255@gmail.com>
2003-11-02 21:31:44 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/branches/UNSTABLE_0_4@18033 72102866-910b-0410-8b05-ffd578937521

---
+++ Modules/RenaissanceProject/PCRenaissanceProj_Prefix.h
@@ -0,0 +1,8 @@
+//
+// Prefix header for all source files of the 'PCRenaissanceProj' target in the 'PCRenaissanceProj' project.
+//
+
+#ifdef __OBJC__
+    #import <Foundation/Foundation.h>
+    #import <AppKit/AppKit.h>
+#endif
