cc6f1ede8b247bc75985027220499d4f7409b0b9
Sergii Stoian <stoyan255@gmail.com>
2004-06-14 09:42:27 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/trunk@19516 72102866-910b-0410-8b05-ffd578937521

---
+++ Modules/AggregateProject/PCAggregateProject.h
@@ -0,0 +1,66 @@
+/*
+   GNUstep ProjectCenter - http://www.gnustep.org
+
+   Copyright (C) 2001 Free Software Foundation
+
+   Authors: Serg Stoyan
+
+   This file is part of GNUstep.
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#ifndef _PCAGGREGATEPROJECT_H
+#define _PCAGGREGATEPROJECT_H
+
+#include <AppKit/AppKit.h>
+#include <ProjectCenter/PCProject.h>
+
+@class PCMakefileFactory;
+
+@interface PCAggregateProject : PCProject
+{
+  IBOutlet NSBox       *projectAttributesView;
+  IBOutlet NSTextField *projectTypeField;
+  IBOutlet NSTextField *projectNameField;
+
+  NSMutableDictionary  *infoDict;
+}
+
+//----------------------------------------------------------------------------
+// Init and free
+//----------------------------------------------------------------------------
+
+- (id)init;
+- (void)dealloc;
+
+@end
+
+@interface PCAggregateProject (GeneratedFiles)
+
+- (BOOL)writeMakefile;
+- (void)appendHead:(PCMakefileFactory *)mff;
+- (void)appendTail:(PCMakefileFactory *)mff;
+
+@end
+
+@interface PCAggregateProject (Inspector)
+
+- (NSView *)projectAttributesView;
+- (void)updateInspectorValues:(NSNotification *)aNotif;
+
+@end
+
+#endif
---
+++ Modules/AggregateProject/PCAggregateProject.m
@@ -0,0 +1,230 @@
+/*
+   GNUstep ProjectCenter - http://www.gnustep.org
+
+   Copyright (C) 2001 Free Software Foundation
+
+   Author: Philippe C.D. Robert <phr@3dkit.org>
+
+   This file is part of GNUstep.
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#include "PCAggregateProject.h"
+#include "PCAggregateProj.h"
+
+#include <ProjectCenter/PCMakefileFactory.h>
+
+@implementation PCAggregateProject
+
+//----------------------------------------------------------------------------
+// Init and free
+//----------------------------------------------------------------------------
+
+- (id)init
+{
+  if ((self = [super init]))
+    {
+      rootKeys = [[NSArray arrayWithObjects:
+	PCSubprojects,
+	PCSupportingFiles,
+	PCNonProject,
+	nil] retain];
+
+      rootCategories = [[NSArray arrayWithObjects:
+	@"Subprojects",
+	@"Supporting Files",
+	@"Non Project Files",
+	nil] retain];
+
+      rootEntries = [[NSDictionary 
+	dictionaryWithObjects:rootCategories forKeys:rootKeys] retain];
+    
+    }
+
+  return self;
+}
+
+- (void)dealloc
+{
+  [rootCategories release];
+  [rootKeys release];
+  [rootEntries release];
+  
+  [super dealloc];
+}
+
+//----------------------------------------------------------------------------
+// Project
+//----------------------------------------------------------------------------
+
+- (Class)builderClass
+{
+  return [PCAggregateProj class];
+}
+
+- (NSString *)projectDescription
+{
+  return @"Project that contains subprojects.";
+}
+
+- (BOOL)isExecutable
+{
+  return NO;
+}
+
+- (NSString *)execAggregateName
+{
+  return nil;
+}
+
+- (NSArray *)fileTypesForCategoryKey:(NSString *)category
+{
+  if ([category isEqualToString:PCSubprojects])
+    {
+      return [NSArray arrayWithObjects:@"subproj",nil];
+    }
+
+  return nil;
+}
+
+- (NSString *)dirForCategory:(NSString *)category
+{
+  return projectPath;
+}
+
+- (NSArray *)buildTargets
+{
+  return nil;
+}
+
+- (NSArray *)sourceFileKeys
+{
+  return nil;
+}
+
+- (NSArray *)resourceFileKeys
+{
+  return nil;
+}
+
+- (NSArray *)otherKeys
+{
+  return [NSArray arrayWithObjects:PCSupportingFiles,nil];
+}
+
+- (NSArray *)allowableSubprojectTypes
+{
+  return [NSArray arrayWithObjects:
+    @"Application", @"Bundle", @"Library", @"Tool", nil];
+}
+
+- (NSArray *)defaultLocalizableKeys
+{
+  return nil;
+}
+
+- (NSArray *)localizableKeys
+{
+  return nil;
+}
+
+@end
+
+@implementation PCAggregateProject (GeneratedFiles)
+
+- (BOOL)writeMakefile
+{
+  PCMakefileFactory *mf = [PCMakefileFactory sharedFactory];
+  NSString          *mfl = nil;
+  NSData            *mfd = nil;
+
+  // Save the GNUmakefile backup
+  [super writeMakefile];
+
+  // Save GNUmakefile.preamble
+  [mf createPreambleForProject:self];
+
+  // Create the new file
+  [mf createMakefileForProject:projectName];
+
+  // Head
+  [self appendHead:mf];
+
+  // Subprojects
+  if ([[projectDict objectForKey:PCSubprojects] count] > 0)
+    {
+      [mf appendSubprojects:[projectDict objectForKey:PCSubprojects]];
+    }
+
+  // Tail
+  [self appendTail:mf];
+
+  // Write the new file to disc!
+  mfl = [projectPath stringByAppendingPathComponent:@"GNUmakefile"];
+  if ((mfd = [mf encodedMakefile])) 
+    {
+      if ([mfd writeToFile:mfl atomically:YES]) 
+	{
+	  return YES;
+	}
+    }
+
+  return NO;
+}
+
+- (void)appendHead:(PCMakefileFactory *)mff
+{
+  [mff appendString:@"\n#\n# Aggregate\n#\n"];
+  [mff appendString:[NSString stringWithFormat:@"PACKAGE_NAME = %@\n",
+    projectName]];
+}
+
+- (void)appendTail:(PCMakefileFactory *)mff
+{
+  [mff appendString:@"\n\n#\n# Makefiles\n#\n"];
+  [mff appendString:@"-include GNUmakefile.preamble\n"];
+  [mff appendString:@"include $(GNUSTEP_MAKEFILES)/aggregate.make\n"];
+  [mff appendString:@"-include GNUmakefile.postamble\n"];
+}
+
+@end
+
+@implementation PCAggregateProject (Inspector)
+
+- (NSView *)projectAttributesView
+{
+  if (projectAttributesView == nil)
+    {
+      if ([NSBundle loadNibNamed:@"Inspector" owner:self] == NO)
+	{
+	  NSLog(@"PCLibraryProject: error loading Inspector NIB!");
+	  return nil;
+	}
+      [projectAttributesView retain];
+      [self updateInspectorValues:nil];
+    }
+
+  return projectAttributesView;
+}
+
+- (void)updateInspectorValues:(NSNotification *)aNotif 
+{
+  [projectTypeField setStringValue:@"Aggregate"];
+  [projectNameField setStringValue:projectName];
+}
+
+@end
+
