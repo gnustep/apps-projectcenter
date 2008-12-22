9feb86a62215d571f29983cc278b25e529864264
Gregory John Casamento <greg.casamento@gmail.com>
2008-12-22 00:31:55 +0000

    Debugger protocol
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/trunk@27392 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/Protocols/CodeDebugger.h
@@ -0,0 +1,60 @@
+/*
+   GNUstep ProjectCenter - http://www.gnustep.org/experience/ProjectCenter.html
+
+   Copyright (C) 2008 Free Software Foundation
+
+   Authors: Gregory Casamento
+
+   This file is part of GNUstep.
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#ifndef _CodeDebugger_h_
+#define _CodeDebugger_h_
+
+#include <Foundation/Foundation.h>
+#include <AppKit/AppKit.h>
+
+@protocol CodeDebugger <NSObject>
+
+// ===========================================================================
+// ==== Initialization
+// ===========================================================================
+
+- (id)initWithPath: (NSString *)filePath;
++ (id)debugExecutableAtPath:(NSString *)filePath;
+- (void)show;
+
+// ===========================================================================
+// ==== Accessor methods
+// ===========================================================================
+
+- (NSWindow *)debuggerWindow;
+- (void)setDebuggerWindow: (NSWindow *)window;
+- (NSView *)debuggerView;
+- (void)setDebuggerView: (id)view;
+- (NSString *)path;
+- (void)setPath:(NSString *)path;
+
+// ===========================================================================
+// ==== Accessor methods
+// ===========================================================================
+
+- (void) startDebugger;
+
+@end
+
+#endif
