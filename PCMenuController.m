feac055af85fc48eeaf4a2d89c03eb7a2942133e
Sergii Stoian <stoyan255@gmail.com>
2009-01-26 23:02:05 +0000

    * Correct mistake in PCPrefController.m file name
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/projectcenter/trunk@27700 72102866-910b-0410-8b05-ffd578937521

--- PCMenuController.m
+++ PCMenuController.m
@@ -19,7 +19,6 @@
    License along with this library; if not, write to the Free
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
 */
-#import <ProjectCenter/PCPrefController.h>
 #import <ProjectCenter/PCLogController.h>
 #import <ProjectCenter/ProjectCenter.h>
 
@@ -28,6 +27,7 @@
 #import "PCAppController.h"
 #import "PCMenuController.h"
 #import "PCInfoController.h"
+#import "PCPrefController.h"
 
 #import "TextFinder.h"
 
